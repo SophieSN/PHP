df94674346a031e83408c44a857d50258b0d996f
Sophie SN <sophie.s.naudet@gmail.com>
2023-06-27 15:31:08 +0200

    operation

--- 01-bases/operation.php
+++ 01-bases/operation.php
@@ -7,11 +7,26 @@
 </head>
 <body>
 
-$a = 5;
-$b = 8;
-$c = 15;
+
+<?php 
+$a = 15;
+$b = 5;
+$c = 8;
+
+$sum = $a + $b + $c;
+echo "$a + $b + $c = $sum \n";
+
+$calcul = $a * ($b - $c);
+echo " $a * ($b - $c) = $calcul \n";
 
 
+$calcul2 = ($c + $b) / $a;
+echo "($c + $b) / $a = $calcul2 \n";
+
+if ($sum<20 or $calcul<20 or $calcul2 <20) {
+    echo"Une des opérations renvoie moins de 20";
+}
+?>
     
 </body>
 </html>
